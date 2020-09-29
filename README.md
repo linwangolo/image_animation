0950ed0e638b98fb28965bb76e4ee84be067046d
Linwang <linwang071993@gmail.com>
2020-09-29 11:39:44 +0800

    add largest face extraction in face detection

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ pip install face-alligment
 
 ## Pre-trained model
 
-Download `vox-adv-cpk.pth.tar` from [here](https://drive.google.com/drive/folders/1PyQJmkdCsAkOYwUyaj_l-l0as-iLDgeH)
+Download `vox-adv-cpk.pth.tar` from [here](https://drive.google.com/drive/folders/1PyQJmkdCsAkOYwUyaj_l-l0as-iLDgeH) and put it in `./`.
 
 
 ## How to use?
@@ -31,18 +31,26 @@ img_ls = [img1, img2, ...]
 ```python
 from image_animation import FaceDetect
 detector = FaceDetect()
-faces = detector.detect(img_ls, crop_size=None, mode = 'Detect_bool')
+boxes, probs, annotates, faces = detector.detect(img_ls, crop_size=None, mode = 'Extract_largest')
 ```
-If you want to see more arguments, use `help(FaceDetect)`.
+If images in list are not in a same size, set `crop_size`.
+There're 4 modes: `Detect_bool`, `Detect`, `Extract_largest`, `Extract_all`.
+Images of faces will be saved in `face_result/faces`.
+If you want to get the annotation image (face boxes on image), set `save_annotate=True` and it will be save in `face_result/annotations`.
+
+Use `help(FaceDetect)` to see more arguments and details.
 
 
 ### Animation
 ```python
 from image_animation import Animation
 animator = Animation()
-animator.animate(SOURCE_VIDEO_PATH, SOURCE_IMAGE_PATH, RESULT_VIDEO_PATH)
+animator.animate(source_video = SOURCE_VIDEO, source_image = 'image_animation/image/einstein.png', result_video = 'result.mp4')
 ```
-`SOURCE_IMAGE_PATH` is the image path of the physicist you want to become.
-And the result will be saved into `RESULT_VIDEO_PATH`.
+`source_video` is the video from camera.
+`source_image` is the image path of the physicist you want to become, defaults to `image_animation/image/einstein.png`.
+And the result will be saved into `result_video`, defaults to `result.mp4`.
 
 
+## Inference time
+The run time is about 29.14s with 1 GPU(GTX 1080).
--- infer.py
+++ infer.py
@@ -1,6 +1,7 @@
 from .crop_video import process_video
 from .demo import demo
 import os
+import numpy as np
 from facenet_pytorch import MTCNN, extract_face
 # import cv2
 from PIL import Image, ImageDraw 
@@ -37,13 +38,14 @@ class Animation:
         self.source_image = source_image
         self.result_video = result_video
         demo(self)
+        os.system('rm crop.mp4')
 
 
 class FaceDetect:
     def __init__(self, thresholds = [0.9, 0.9, 0.9], min_face_size = 100):
         self.mtcnn = MTCNN(thresholds=thresholds, select_largest=True, post_process=False, device='cuda:0', min_face_size=min_face_size)
     
-    def detect(self,img_ls, crop_size = None, mode = 'Detect_bool', save_faces = False, save_annotate = False):
+    def detect(self, img_ls, crop_size = None, mode = 'Extract_largest', save_faces = False, save_annotate = False, save_path = 'face_result'):
         """face detection
 
         Args:
@@ -75,51 +77,67 @@ class FaceDetect:
             return isinstance(boxes, np.ndarray)
         elif mode == 'Detect':
             return boxes, probs 
-        elif mode == 'Extract':
+        elif 'Extract' in mode:
             faces = []
             annotates = []
+            boxes = boxes.tolist()
+            probs = probs.tolist()
             for id_, img in enumerate(img_ls):
                 face_batch = []
                 img_annotate = img.copy()
                 draw = ImageDraw.Draw(img_annotate)
-                for i, box in enumerate(boxes[id_]):
+                box_all = boxes[id_]                
+                if mode == 'Extract_largest':
+                    for i, box in enumerate(box_all):
+                        left = max(0, box[0])
+                        top = max(0, box[1])
+                        right = min(np.array(img_ls[id_]).shape[1], box[2])
+                        down = min(np.array(img_ls[id_]).shape[0], box[3])
+                        box_all[i] = [left, top, right, down]
+                    area = list(map(self._cal_area, box_all))
+                    max_id = area.index(max(area))
+                    box = box_all[max_id]
                     box_head = [box[0]-box[0]/8, box[1]-box[1]/5, box[2]+box[2]/8, box[3]+box[3]/10]
-                    boxes[id_][i] = np.asarray(box_head)
-                    draw.rectangle(box_head, width=5)  # box.tolist()
+                    boxes[id_] = [box_head]
+                    probs[id_] = [probs[id_][max_id]]
+
+                    draw.rectangle(box_head, width=5)
                     if save_faces:
-                        face_batch.append(extract_face(img, box_head, save_path=os.path.join(path,f'detected_face_{id_}-{i}.png')))
+                        if not os.path.exists(save_path):
+                            os.mkdir(save_path)
+                        if not os.path.exists(os.path.join(save_path, 'faces')):
+                            os.mkdir(os.path.join(save_path, 'faces'))
+                        face_batch.append(extract_face(img, box_head, save_path=os.path.join(save_path,f'detected_face_{id_}-{0}.png')))
                     else:
                         face_batch.append(extract_face(img, box_head))
+                elif mode == 'Extract_all':
+                    for i, box in enumerate(box_all):
+                        box_head = [box[0]-box[0]/3, box[1]-box[1]/3, box[2]+box[2]/83, box[3]+box[3]/10]
+                        box_all[i] = box_head
+                        draw.rectangle(box_head, width=5)  # box.tolist()
+                        if save_faces:
+                            if not os.path.exists(save_path):
+                            os.mkdir(save_path)
+                        if not os.path.exists(os.path.join(save_path, 'faces')):
+                            os.mkdir(os.path.join(save_path, 'faces'))
+                            face_batch.append(extract_face(img, box_head, save_path=os.path.join(save_path,f'detected_face_{id_}-{i}.png')))
+                        else:
+                            face_batch.append(extract_face(img, box_head))
+                else:
+                    print(f"Error: there's no mode called {mode}")
                 faces.append(face_batch)
                 annotates.append(np.asarray(img_annotate))
                 if save_annotate:
-                    img_annotate.save(os.path.join(path,f'annotated_faces_{id_}.png'))
-            return boxes, probs, annotates, faces
+                    if not os.path.exists(save_path):
+                        os.mkdir(save_path)
+                    if not os.path.exists(os.path.join(save_path, 'annotations')):
+                        os.mkdir(os.path.join(save_path, 'annotations'))
+                    img_annotate.save(os.path.join(save_path,f'annotated_faces_{id_}.png'))
+            return np.asarray(boxes), probs, annotates, faces
         else:
             print(f"Error: there's no mode called {mode}")
 
-
-
-if __name__ == "__main__":
-    a = Animation()
-    a.crop_video()
-    a.animate()
-
-
-
-    # import images
-    path = 'data_faces_detect'
-    files = os.listdir(path)
-    images = []
-    for f in files:    
-        print(f)
-        img = Image.open(os.path.join(path,f))
-        images.append(img)
-
-
-# have_face = detect(images, crop_size = (0,0,512,341))  
-# boxes, probs = detect(images,mode='Detect', crop_size = (0,0,512,341)) 
-# boxes, probs, annotates, faces = detect(images, mode='Extract', crop_size = (0,0,512,341)) #, face_extract = True, save_faces = True, save_annotate = True)
-# detect(images_2, mode='Extract',  save_faces = True, save_annotate = True)
+    def _cal_area(self, ls):
+        return (ls[2]-ls[0])*(ls[3]-ls[1])
 
 
